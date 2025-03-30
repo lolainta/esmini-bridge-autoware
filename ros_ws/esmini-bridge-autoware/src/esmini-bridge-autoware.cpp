f0043e5fb2e1362be704cff68a14d2abd25c71ad
LolaInTa Chi <lolainta@gmail.com>
2025-03-31 03:16:31 +0800

    feat: esmini_bridge_autoware py package

--- ros_ws/esmini-bridge-autoware/src/esmini-bridge-autoware.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-
-    printf("hello world esmini-bridge-autoware package\n");
-    return 0;
-}
---
+++ ros_ws/src/esmini_bridge_autoware/esmini_bridge_autoware/esmini_bridge_autoware.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from esmini_bridge_autoware.')
+
+
+if __name__ == '__main__':
+    main()
